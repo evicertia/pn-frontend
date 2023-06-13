ee4030fadd652cef0d47ed70cc5c3667e6fd9573
elena.ibba <elena.ibba@accenture.com>
2023-06-13 12:20:54 +0200

    Test Card e reducer estimete

--- packages/pn-pa-webapp/src/pages/components/UsageEstimates/actualEstimateCard/__test__/ActualEstimateCard.test.tsx
+++ packages/pn-pa-webapp/src/pages/components/UsageEstimates/actualEstimateCard/__test__/ActualEstimateCard.test.tsx
@@ -10,130 +10,10 @@ import {GET_EDIT_ESTIMATE_PATH} from "../../../../../navigation/routes.const";
 import * as reactRedux from "../../../../../redux/hooks";
 
 
-const mockNavigateFn = jest.fn();
-
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockNavigateFn,
-}));
-
-describe('ActualEstimateCard', () => {
-    const mockDispatchFn = jest.fn();
-    const spyOnDispatch = jest.spyOn(reactRedux, "useAppDispatch");
-
-    beforeEach(() => {
-
-    });
-
-    afterEach(() => {
-        cleanup();
-    });
-    const props = {
-        paId: 'paId1234567',
-        data: {...draftElement
-        },
-    };
-
-
-    it('renders create estimate button when lastModifiedDate is null and status is DRAFT', async () => {
-
-        render(<BrowserRouter >
-            <Routes>
-                <Route path={"/"} element={ <ActualEstimateCard {...props}/>} />
-                <Route path={GET_EDIT_ESTIMATE_PATH(draftElement.referenceMonth)} element={<h1 data-testid={"estimate-edit-page"}>Estimate page route</h1>}/>
-            </Routes>
-        </BrowserRouter>);
-
-
-        const estimateButton = screen.getByTestId("create-button-test-id");
-        fireEvent.click(estimateButton);
-
-
-        await act(async () => {
-            await waitFor( () => {
-                expect(location.pathname).toEqual(GET_EDIT_ESTIMATE_PATH(draftElement.referenceMonth));
-                expect(screen.getByTestId("estimate-edit-page")).toBeInTheDocument()
-                //expect(mockDispatchFn).toBeCalledTimes(0);
-                expect(mockNavigateFn).toBeCalledTimes(1);
-            })
-
-        })
-    });
-
-    it('edit estimate button and send estimate button when status is DRAFT', () => {
-        spyOnDispatch.mockReturnValue(mockDispatchFn);
-        const props_test = {
-            paId: 'paId',
-            data: {
-                lastModifiedDate: '2023-05-22T13:36:27.000+00:00',
-                status: EstimateStatusEnum.DRAFT,
-                referenceMonth: 'GIU-2023',
-            },
-        };
-
-        const { getByText } = render(
-            <ActualEstimateCard {...props}  />
-        );
-
-        const editEstimateButton = getByText('Modifica stima');
-        fireEvent.click(editEstimateButton);
-
-        expect(mockNavigateFn).toHaveBeenCalledWith(
-            GET_EDIT_ESTIMATE_PATH(props.data.referenceMonth)
-        );
-
-        const sendEstimateButton = getByText('Invia stima');
-        fireEvent.click(sendEstimateButton);
-
-        //
-
-    });
-
-    it('edit estimate button when status is VALIDATED', () => {
-        spyOnDispatch.mockReturnValue(mockDispatchFn);
-        const props_test = {
-            paId: 'paId',
-            data: {
-                lastModifiedDate: null,
-                status: EstimateStatusEnum.VALIDATED,
-                referenceMonth: 'GIU-2023',
-            },
-        };
-
-        const { getByText } = render(
-            <ActualEstimateCard {...props}  />
-        );
-
-        const editEstimateButton = getByText('Modifica stima');
-        fireEvent.click(editEstimateButton);
-
-        expect(mockNavigateFn).toHaveBeenCalledWith(
-            GET_EDIT_ESTIMATE_PATH(props.data.referenceMonth)
-        );
-    });
-
-    it('renders null when none of the conditions are met', () => {
-        spyOnDispatch.mockReturnValue(mockDispatchFn);
-        const props_test = {
-            paId: 'paId1234567',
-            data: {
-                lastModifiedDate: '2023-05-22T13:36:27.000+00:00',
-                status: EstimateStatusEnum.DRAFT,
-                referenceMonth: 'GIU-2023',
-            },
-        };
-
-        const { container } = render(
-            <ActualEstimateCard {...props}  />
-        );
-
-        expect(container.firstChild).toBeNull();
-    });
-});
 
 
 
-const draftElement :EstimatePeriod = {
+const firstEstimate :EstimatePeriod = {
     referenceMonth: "LUG-2023",
     status: EstimateStatusEnum.DRAFT,
     lastModifiedDate: null,
@@ -154,11 +34,34 @@ const draftElement :EstimatePeriod = {
 
 };
 
-const validatedElement :EstimatePeriod = {
+
+const draftEstimate :EstimatePeriod = {
     referenceMonth: "GIU-2023",
     status: EstimateStatusEnum.DRAFT,
     lastModifiedDate: "2023-04-22T13:36:27.000+00:00",
     deadlineDate: "2023-05-15T23:59:00.000+00:00",
+    estimate: {
+        totalDigitalNotif: 5,
+        total890Notif: 10,
+        totalAnalogNotif: 15,
+    },
+    showEdit: false,
+    billing: {
+        sdiCode: "ABCDE12345",
+        splitPayment: false,
+        description: "string",
+        mailAddress: "test@test.com",
+
+    },
+
+};
+
+
+const validatedEstimate :EstimatePeriod = {
+    referenceMonth: "GIU-2023",
+    status: EstimateStatusEnum.DRAFT,
+    lastModifiedDate: "2023-05-22T13:36:27.000+00:00",
+    deadlineDate: "2023-06-15T23:59:00.000+00:00",
     estimate: {
         totalDigitalNotif: 12,
         total890Notif: 1330,
@@ -175,87 +78,175 @@ const validatedElement :EstimatePeriod = {
 
 };
 
-const firstEstimate :EstimatePeriod = {
-    referenceMonth: "GIU-2023",
-    status: EstimateStatusEnum.DRAFT,
-    lastModifiedDate: "2023-05-22T13:36:27.000+00:00",
-    deadlineDate: "2023-06-15T23:59:00.000+00:00",
-    estimate: {
-        totalDigitalNotif: 0,
-        total890Notif: 0,
-        totalAnalogNotif: 0,
-    },
-    showEdit: false,
-    billing: {
-        sdiCode: "ABCDE12345",
-        splitPayment: false,
-        description: "string",
-        mailAddress: "test@test.com",
 
-    },
 
-};
-
-const propsDraftElement ={
+const propsDraftEstimate ={
     paId:"123456789",
-    data: draftElement,
+    data: draftEstimate,
 };
 
 const propsValidateElement ={
     paId:"123456789",
-    data: validatedElement,
+    data: validatedEstimate,
 };
 
-const propsFirstElement ={
+const propsFirstEstimate ={
     paId:"123456789",
     data: firstEstimate,
 };
 
+const propsNullElement ={
+    paId: null,
+    data: firstEstimate,
+};
 
-const mockStore = createStore(()=> propsDraftElement, propsDraftElement);
-describe("ActualEstimateCard", () => {
-    afterEach(cleanup);
-    it("renders the card with correct data - DRAFT", async () => {
-        render(
-            <Provider store={mockStore}>
-                    <MemoryRouter>
-                        <ActualEstimateCard {...propsDraftElement} />
-                    </MemoryRouter>
-            </Provider>
-        );
 
-        expect(screen.getByText("5")).toBeInTheDocument();
-        expect(screen.getByText("10")).toBeInTheDocument();
-        expect(screen.getByText("15")).toBeInTheDocument();
+describe('ActualEstimateCardFunctionalities', () => {
+
+    afterEach(() => {
+        cleanup();
+    });
+
+
+    it('Create estimate button behaviour when lastModifiedDate is null and status is DRAFT', async () => {
+        render(<BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<ActualEstimateCard {...propsFirstEstimate}/>}/>
+                <Route path={GET_EDIT_ESTIMATE_PATH(propsFirstEstimate.data.referenceMonth)}
+                       element={<h1 data-testid={"estimate-edit-page"}>Estimate page route</h1>}/>
+            </Routes>
+        </BrowserRouter>);
+        const estimateButton = screen.getByTestId("create-button-test-id");
+        fireEvent.click(estimateButton);
+        await act(async () => {
+            await waitFor(() => {
+                expect(location.pathname).toEqual(GET_EDIT_ESTIMATE_PATH(propsFirstEstimate.data.referenceMonth));
+                expect(screen.getByTestId("estimate-edit-page")).toBeInTheDocument()
+            })
+
+        })
+    });
+
+
+    it('renders create estimate button when lastModifiedDate is not null and status is DRAFT', async () => {
+        const mockNavigateFn = jest.fn();
+        jest.mock('react-router-dom', () => ({
+            ...jest.requireActual('react-router-dom'),
+            useNavigate: () => mockNavigateFn,
+        }));
+        const mockDispatchFn = jest.fn();
+        const spyOnDispatch = jest.spyOn(reactRedux, "useAppDispatch");
+        spyOnDispatch.mockReturnValue(mockDispatchFn);
+        render(<BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<ActualEstimateCard {...propsDraftEstimate}/>}/>
+                <Route path={GET_EDIT_ESTIMATE_PATH(propsDraftEstimate.data.referenceMonth)}
+                       element={<h1 data-testid={"estimate-edit-page"}>Estimate page route</h1>}/>
+            </Routes>
+        </BrowserRouter>);
+        const estimateButton = screen.getByText("actual-estimate.card.button.edit-estimate");
+        fireEvent.click(estimateButton);
+        await act(async () => {
+            await waitFor(() => {
+                expect(location.pathname).toEqual(GET_EDIT_ESTIMATE_PATH(propsDraftEstimate.data.referenceMonth));
+                expect(screen.getByTestId("estimate-edit-page")).toBeInTheDocument()
+            })
+        })
+    });
+
+
+    it('edit estimate button and send estimate button when status is DRAFT', async () => {
+        const mockDispatchFn = jest.fn();
+        const spyOnDispatch = jest.spyOn(reactRedux, "useAppDispatch");
+        spyOnDispatch.mockReturnValue(mockDispatchFn);
+
+        const {getByText} = render(<BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<ActualEstimateCard {...propsDraftEstimate}/>}/>
+            </Routes>
+        </BrowserRouter>);
+
+        const sendEstimateButton = getByText('actual-estimate.card.button.send-estimate');
+        fireEvent.click(sendEstimateButton);
+
+        await act(async () => {
+            await waitFor(() => {
+                expect(screen.getByTestId("send-estimate-dialog")).toBeInTheDocument()
+            })
+        })
 
     });
 
-    it("renders the card with correct data - VALIDATED", async () => {
-         render(
-               <Provider store={mockStore}>
+
+    it('edit estimate button when status is VALIDATED', async () => {
+        const mockDispatchFn = jest.fn();
+        const spyOnDispatch = jest.spyOn(reactRedux, "useAppDispatch");
+        spyOnDispatch.mockReturnValue(mockDispatchFn);
+
+        const {getByText} = render(<BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<ActualEstimateCard {...propsValidateElement}/>}/>
+                <Route path={GET_EDIT_ESTIMATE_PATH(propsValidateElement.data.referenceMonth)}
+                       element={<h1 data-testid={"estimate-edit-page"}>Estimate page route</h1>}/>
+            </Routes>
+        </BrowserRouter>);
+
+        const editEstimateButton = screen.getByText("actual-estimate.card.button.edit-estimate");
+        fireEvent.click(editEstimateButton);
+        await act(async () => {
+            await waitFor(() => {
+                expect(location.pathname).toEqual(GET_EDIT_ESTIMATE_PATH(propsValidateElement.data.referenceMonth));
+                expect(screen.getByTestId("estimate-edit-page")).toBeInTheDocument()
+            })
+        })
+    });
+
+});
+    const mockStore = createStore(() => propsDraftEstimate, propsDraftEstimate);
+    describe("ActualEstimateCardRender", () => {
+        afterEach(cleanup);
+
+        it("renders the card with correct data - DRAFT", async () => {
+            render(
+                <Provider store={mockStore}>
+                    <MemoryRouter>
+                        <ActualEstimateCard {...propsDraftEstimate} />
+                    </MemoryRouter>
+                </Provider>
+            );
+
+            expect(screen.getByText("5")).toBeInTheDocument();
+            expect(screen.getByText("10")).toBeInTheDocument();
+            expect(screen.getByText("15")).toBeInTheDocument();
+
+        });
+
+        it("renders the card with correct data - VALIDATED", async () => {
+            render(
+                <Provider store={mockStore}>
                     <MemoryRouter>
                         <ActualEstimateCard {...propsValidateElement} />
                     </MemoryRouter>
                 </Provider>
-         );
+            );
 
-         expect(screen.getByText("12")).toBeInTheDocument();
-         expect(screen.getByText("1330")).toBeInTheDocument();
-         expect(screen.getByText("125")).toBeInTheDocument();
+            expect(screen.getByText("12")).toBeInTheDocument();
+            expect(screen.getByText("1330")).toBeInTheDocument();
+            expect(screen.getByText("125")).toBeInTheDocument();
+
+        });
+
+        it("renders the card with correct data - First estimation", async () => {
+            render(
+                <Provider store={mockStore}>
+                    <MemoryRouter>
+                        <ActualEstimateCard {...propsFirstEstimate} />
+                    </MemoryRouter>
+                </Provider>
+            );
+            expect((screen.queryByTestId("testIdTagEditDate"))).not.toBeInTheDocument();
+
+        });
 
     });
-
-    it("renders the card with correct data - First estimation", async () => {
-        render(
-            <Provider store={mockStore}>
-                <MemoryRouter>
-                    <ActualEstimateCard {...propsFirstElement} />
-                </MemoryRouter>
-            </Provider>
-        );
-        expect( (screen.queryByTestId("testIdTagEditDate"))).not.toBeInTheDocument();
-
-    });
-
-});
 
